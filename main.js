a61bce87db2bc3fe96317e10bc70ef34bfe44c5b
Misti786 <skmanowar690@gmail.com>
2020-12-11 15:01:04 +0530

    TicTacTio

---
+++ main.js
@@ -0,0 +1,546 @@
+function fun1() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box1');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+
+    }
+  }
+}
+
+function fun2() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box2');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+
+function fun3() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box3');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun4() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box4');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun5() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box5')
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×'
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun6() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box6');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun7() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box7');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun8() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box8');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+function fun9() {
+  var reFresh = setInterval(check, 0)
+
+  function check() {
+    var x = document.querySelector('h1').innerHTML;
+    var a = document.querySelector('#box9');
+    a.style.zIndex = '1';
+
+    if (x == 2) {
+      a.innerHTML = '×';
+      a.style.color = '#49F024';
+      a.style.textShadow = '0 0 5px #49F024, 0 0 20px #49F024';
+      document.querySelector('h1').innerHTML = '1'
+      clearInterval(reFresh)
+
+    }
+    else if (x == 1) {
+      a.innerHTML = 'o'
+      a.style.color = '#FF2828';
+      a.style.textShadow = '0 0 5px #FF2828 , 0 0 20px #FF2828';
+      document.querySelector('h1').innerHTML = '2'
+      clearInterval(reFresh)
+    }
+  }
+}
+
+var fresh = setInterval(check, 1)
+
+function check() {
+  var b1 = document.querySelector('#box1').innerHTML;
+  var b2 = document.querySelector('#box2').innerHTML;
+  var b3 = document.querySelector('#box3').innerHTML;
+  var b4 = document.querySelector('#box4').innerHTML;
+  var b5 = document.querySelector('#box5').innerHTML;
+  var b6 = document.querySelector('#box6').innerHTML;
+  var b7 = document.querySelector('#box7').innerHTML;
+  var b8 = document.querySelector('#box8').innerHTML;
+  var b9 = document.querySelector('#box9').innerHTML;
+
+  //Horizontal Rows ➡
+  //                ➡
+  //                ➡
+  var lineContainer = document.querySelector('#winnerLineContainer');
+  var winnerListx = document.querySelector('#winnerListx');
+  var blur = document.querySelector('#blur');
+
+  if (b1 == '×' && b2 == '×' && b3 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr1').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr1').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b4 == '×' && b5 == '×' && b6 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr2').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr2').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b7 == '×' && b8 == '×' && b9 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr3').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr3').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  ////////////////
+
+  //Vertical Rows ⬇⬇⬇
+  if (b1 == '×' && b4 == '×' && b7 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr1').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr1').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b2 == '×' && b5 == '×' && b8 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr2').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr2').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b3 == '×' && b6 == '×' && b9 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr3').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr3').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+  //////////////
+
+  //Cross ↘↗
+  if (b1 == '×' && b5 == '×' && b9 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineCr1').style.transform = 'rotate(-45deg)' + 'scale(1)';
+    document.querySelector('#winnerLineCr1').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b7 == '×' && b5 == '×' && b3 == '×') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineCr2').style.transform = 'rotate(45deg)' + 'scale(1)';
+    document.querySelector('#winnerLineCr2').style.background = '#49F024';
+    winnerListx.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+  //////
+
+  var winnerListo = document.querySelector('#winnerListo');
+  //Horizontal Rows ➡
+  //                ➡
+  //                ➡
+  if (b1 == 'o' && b2 == 'o' && b3 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr1').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr1').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b4 == 'o' && b5 == 'o' && b6 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr2').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr2').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b7 == 'o' && b8 == 'o' && b9 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineHr3').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineHr3').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+  ////////////////
+
+  //Vertical Rows ⬇⬇⬇
+  if (b1 == 'o' && b4 == 'o' && b7 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr1').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr1').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b2 == 'o' && b5 == 'o' && b8 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr2').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr2').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b3 == 'o' && b6 == 'o' && b9 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineVr3').style.transform = 'scale(1)';
+    document.querySelector('#winnerLineVr3').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+  //////////////
+
+  //Cross ↘↗
+  if (b1 == 'o' && b5 == 'o' && b9 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineCr1').style.transform = 'rotate(-45deg)' + 'scale(1)';
+    document.querySelector('#winnerLineCr1').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+
+  if (b7 == 'o' && b5 == 'o' && b3 == 'o') {
+    lineContainer.style.zIndex = '2';
+    document.querySelector('#winnerLineCr2').style.transform = 'rotate(45deg)' + 'scale(1)';
+    document.querySelector('#winnerLineCr2').style.background = '#FF2828';
+    winnerListo.style.transform = 'scale(1)';
+    blur.style.filter = 'blur(10px)';
+  }
+  //////
+
+  ///Drow/////
+
+  if (b1 == 'o' || b1 == '×') {
+    if (b2 == 'o' || b2 == '×') {
+      if (b3 == 'o' || b3 == '×') {
+        if (b4 == 'o' || b4 == '×') {
+          if (b5 == 'o' || b5 == '×') {
+            if (b6 == 'o' || b6 == '×') {
+              if (b7 == 'o' || b7 == '×') {
+                if (b8 == 'o' || b8 == '×') {
+                  if (b9 == 'o' || b9 == '×') {
+
+                    document.querySelector('#winnerListDrow').style.transform = 'scale(1)';
+                    blur.style.filter = 'blur(10px)';
+
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+//////Rest button function////
+function rest() {
+  document.querySelector('#box1').innerHTML = null;
+  document.querySelector('#box2').innerHTML = null;
+  document.querySelector('#box3').innerHTML = null;
+  document.querySelector('#box4').innerHTML = null;
+  document.querySelector('#box5').innerHTML = null;
+  document.querySelector('#box6').innerHTML = null;
+  document.querySelector('#box7').innerHTML = null;
+  document.querySelector('#box8').innerHTML = null;
+  document.querySelector('#box9').innerHTML = null;
+
+  document.querySelector('#box1').style.zIndex = '2';
+  document.querySelector('#box2').style.zIndex = '2';
+  document.querySelector('#box3').style.zIndex = '2';
+  document.querySelector('#box4').style.zIndex = '2';
+  document.querySelector('#box5').style.zIndex = '2';
+  document.querySelector('#box6').style.zIndex = '2';
+  document.querySelector('#box7').style.zIndex = '2';
+  document.querySelector('#box8').style.zIndex = '2';
+  document.querySelector('#box9').style.zIndex = '2';
+
+  document.querySelector('#winnerLineContainer').style.zIndex = '1';
+  document.querySelector('#winnerLineHr1').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineHr2').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineHr3').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineVr1').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineVr2').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineVr3').style.transform = 'scale(0)';
+  document.querySelector('#winnerLineCr1').style.transform = 'rotate(-45deg)' + 'scale(0)';
+  document.querySelector('#winnerLineCr2').style.transform = 'rotate(45deg)' + 'scale(0)';
+
+  document.querySelector('#winnerListx').style.transform = 'scale(0)'
+  document.querySelector('#winnerListo').style.transform = 'scale(0)'
+  document.querySelector('#winnerListDrow').style.transform = 'scale(0)'
+
+  document.querySelector('#blur').style.filter = 'blur(0px)'
+
+  //// ×/o Point calculate no:_1////
+  var point = setInterval(pointxo, 500)
+function pointxo() {
+  var pointx = document.querySelector('#winnerListx').style.transform;
+var pointo = document.querySelector('#winnerListo').style.transform;
+
+  if (pointx == 'scale(1)') {
+    var b = +document.querySelector('#pointx').innerHTML
+    var a = +1 + b
+    document.querySelector('#pointx').innerHTML = a
+    clearInterval(point)
+  }
+  if (pointo == 'scale(1)') {
+    var b = +document.querySelector('#pointo').innerHTML
+    var a = +1 + b
+    document.querySelector('#pointo').innerHTML = a
+    clearInterval(point)
+  }
+}
+  /////////
+}
+
+//////////
+/// ×/o TURN display ////
+setInterval(function() {
+  var target1 = document.querySelector('#winnerLineContainer').style.zIndex;
+  var target2 = document.querySelector('#winnerListDrow').style.transform;
+
+  var turn = document.querySelector('h1').innerHTML;
+  if (turn == 1) {
+    document.querySelector('h2').innerHTML = "'o' TURN"
+    document.querySelector('h2').style.color = '#FF2828'
+  }
+  if (turn == 2) {
+    document.querySelector('h2').innerHTML = "'×' TURN"
+    document.querySelector('h2').style.color = '#49F024';
+  }
+  if (target1 == '2' || target2 == 'scale(1)') {
+    document.querySelector('h2').innerHTML = null
+  }
+}, 1)
+///////////
+
+//// ×/o Point calculate no:_2 (no1 lineNumber:465)////
+var point = setInterval(pointxo, 500)
+function pointxo() {
+  var pointx = document.querySelector('#winnerListx').style.transform;
+var pointo = document.querySelector('#winnerListo').style.transform;
+
+  if (pointx == 'scale(1)') {
+    var b = +document.querySelector('#pointx').innerHTML
+    var a = +1 + b
+    document.querySelector('#pointx').innerHTML = a
+    clearInterval(point)
+  }
+  if (pointo == 'scale(1)') {
+    var b = +document.querySelector('#pointo').innerHTML
+    var a = +1 + b
+    document.querySelector('#pointo').innerHTML = a
+    clearInterval(point)
+  }
+}
+
+////pointRest////
+function pointRest(){
+  document.querySelector('#pointx').innerHTML=0
+  document.querySelector('#pointo').innerHTML=0
+}
+
+
+
+////
+var load=setInterval(loading,4100)
+function loading(){
+  document.querySelector("#loadingContainer").style.transform='translateY(-800px)';
+  clearInterval(load)
+};
+var start=setInterval(startGame,4200)
+function startGame(){
+  document.querySelector("#startGame").style.display='block';
+  clearInterval(start)
+};
